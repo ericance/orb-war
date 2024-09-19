873fa26ff2dd5b1660be7e747209ec357362138d
Eric <ericstalcup@gmail.com>
2024-09-19 04:56:37 -0500

    Detect when player is hit, remove off screen projectiles, and colorize the enemies

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ class Enemy {
 	}
 }
 
-const player = new Player(canvas.width / 2, canvas.height / 2, 30, '#000')
+const player = new Player(canvas.width / 2, canvas.height / 2, 30, '#fff')
 const projectiles = []
 const enemies = []
 
@@ -85,9 +85,8 @@ function spawnEnemies() {
 		} else {
 			x = Math.random() * canvas.width;
 			y = Math.random() < 0.5 ? - radius : canvas.height + radius;
-
 		}
-		const color = 'red';
+		const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
 		const angle = Math.atan2(
 			canvas.height / 2 - y,
 			canvas.width / 2 - x
@@ -100,23 +99,41 @@ function spawnEnemies() {
 	}, 1000);
 }
 
+let animationId
 function animate() {
-	requestAnimationFrame(animate)
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	animationId = requestAnimationFrame(animate)
+	ctx.fillStyle = 'rgba(0,0,0,.1)'
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	player.draw();
-	projectiles.forEach(projectile => {
+	projectiles.forEach((projectile, index) => {
 		projectile.update()
+
+		// remove from edges of screen
+		if (
+			projectile.x + projectile.radius < 0 ||
+			projectile.x - projectile.radius > canvas.width ||
+			projectile.y + projectile.radius < 0 ||
+			projectile.y - projectile.radius > canvas.height
+		) {
+			setTimeout(() => {
+				projectiles.splice(index, 1)
+			}, 0);
+		}
 	})
 
 	enemies.forEach((enemy, index) => {
 		enemy.update()
 
+		const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+		// end game
+		if (dist - enemy.radius - player.radius < 1) {
+			cancelAnimationFrame(animationId)
+		}
+
 		projectiles.forEach((projectile, projectileIndex) => {
 			// (why hypot???)
-			const dist = Math.hypot(
-				projectile.x - enemy.x,
-				projectile.y - enemy.y
-			)
+			const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
 			// objects touch
 			if (dist - enemy.radius - projectile.radius < 1) {
@@ -130,16 +147,17 @@ function animate() {
 }
 
 addEventListener('click', (object) => {
+	console.log(projectiles)
 	const angle = Math.atan2(
 		object.clientY - canvas.height / 2,
 		object.clientX - canvas.width / 2
 	);
 	const velocity = {
-		x: Math.cos(angle),
-		y: Math.sin(angle),
+		x: Math.cos(angle) * 6,
+		y: Math.sin(angle) * 6,
 	}
 	projectiles.push(
-		new Projectile(canvas.width / 2, canvas.height / 2, 2, 'red', velocity)
+		new Projectile(canvas.width / 2, canvas.height / 2, 5, '#fff', velocity)
 	)
 })
 
