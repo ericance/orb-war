83cfe3871a748331b052dbc3399e1100714895de
ericance <ericstalcup@gmail.com>
2024-09-21 05:21:39 -0500

    Connect and disconnect players

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('canvas');
 var ctx = canvas.getContext("2d");
 
+const socket = io()
+
 canvas.width = innerWidth
 canvas.height = innerHeight
 
@@ -9,131 +11,37 @@ const scoreElement = document.querySelector('#scoreElement');
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-let player = new Player(x, y, 30, '#fff')
-let projectiles = []
-let particles = []
-let enemies = []
+const player = new Player(x, y, 20, '#fff')
+const players = {}
+socket.on('updatePlayers', (backendPlayers) => {
+	for (const id in backendPlayers) {
+		const backendPlayer = backendPlayers[id]
 
-function spawnEnemies() {
-	setInterval(() => {
-		const radius = Math.random() * (30 - 5) + 5;
-		let x
-		let y
+		if (!players[id]) {
+			players[id] = new Player(backendPlayer.x, backendPlayer.y, 20, '#fff')
+		}
+	}
 
-		// 50% chance to set y to 0
-		if (Math.random() < 0.5) {
-			x = Math.random() < 0.5 ? - radius : canvas.width + radius;
-			y = Math.random() * canvas.height;
-		} else {
-			x = Math.random() * canvas.width;
-			y = Math.random() < 0.5 ? - radius : canvas.height + radius;
+	for (const id in players) {
+		if (!backendPlayers[id]) {
+			delete players[id]
 		}
-		const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
-		const angle = Math.atan2(
-			canvas.height / 2 - y,
-			canvas.width / 2 - x
-		);
-		const velocity = {
-			x: Math.cos(angle),
-			y: Math.sin(angle),
-		}
-		enemies.push(new Enemy(x, y, radius, color, velocity))
-	}, 1000);
-}
+	}
+
+	console.log(players)
+})
 
 let animationId
-let score = 0
 function animate() {
 	animationId = requestAnimationFrame(animate)
 	ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
 	ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-	player.draw()
-
-	for (let index = particles.length - 1; index >= 0; index--) {
-		const particle = particles[index]
-
-		if (particle.alpha <= 0) {
-			particles.splice(index, 1)
-		} else {
-			particle.update()
-		}
+	for (const id in players) {
+		const player = players[id]
+		player.draw()
 	}
 
-	for (let index = projectiles.length - 1; index >= 0; index--) {
-		const projectile = projectiles[index]
-
-		projectile.update()
-
-		// remove from edges of screen
-		if (
-			projectile.x - projectile.radius < 0 ||
-			projectile.x - projectile.radius > canvas.width ||
-			projectile.y + projectile.radius < 0 ||
-			projectile.y - projectile.radius > canvas.height
-		) {
-			projectiles.splice(index, 1)
-		}
-	}
-
-	for (let index = enemies.length - 1; index >= 0; index--) {
-		const enemy = enemies[index]
-
-		enemy.update()
-
-		const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
-
-		//end game
-		if (dist - enemy.radius - player.radius < 1) {
-			cancelAnimationFrame(animationId)
-		}
-
-		for (
-			let projectilesIndex = projectiles.length - 1;
-			projectilesIndex >= 0;
-			projectilesIndex--
-		) {
-			const projectile = projectiles[projectilesIndex]
-
-			const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
-
-			// when projectiles touch enemy
-			if (dist - enemy.radius - projectile.radius < 1) {
-				// create explosions
-				for (let i = 0; i < enemy.radius * 2; i++) {
-					particles.push(
-						new Particle(
-							projectile.x,
-							projectile.y,
-							Math.random() * 2,
-							enemy.color,
-							{
-								x: (Math.random() - 0.5) * (Math.random() * 6),
-								y: (Math.random() - 0.5) * (Math.random() * 6)
-							}
-						)
-					)
-				}
-				// this is where we shrink our enemy
-				if (enemy.radius - 10 > 5) {
-					score += 100
-					scoreEl.innerHTML = score
-					gsap.to(enemy, {
-						radius: enemy.radius - 10
-					})
-					projectiles.splice(projectilesIndex, 1)
-				} else {
-					// remove enemy if they are too small
-					score += 150
-					scoreEl.innerHTML = score
-
-					enemies.splice(index, 1)
-					projectiles.splice(projectilesIndex, 1)
-				}
-			}
-		}
-	}
 }
 
 animate()
-spawnEnemies()
