e508d9bc79b043f0da00f95751482582967179ef
ericance <ericstalcup@gmail.com>
2024-09-21 05:43:28 -0500

    Refactor front and back end players

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -11,14 +11,14 @@ const scoreElement = document.querySelector('#scoreElement');
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const players = {}
+const frontEndPlayers = {}
 
-socket.on('updatePlayers', (backendPlayers) => {
-	for (const id in backendPlayers) {
-		const backendPlayer = backendPlayers[id]
+socket.on('updatePlayers', (backEndPlayers) => {
+	for (const id in backEndPlayers) {
+		const backendPlayer = backEndPlayers[id]
 
-		if (!players[id]) {
-			players[id] = new Player({
+		if (!frontEndPlayers[id]) {
+			frontEndPlayers[id] = new Player({
 				x: backendPlayer.x,
 				y: backendPlayer.y,
 				radius: 20,
@@ -27,13 +27,11 @@ socket.on('updatePlayers', (backendPlayers) => {
 		}
 	}
 
-	for (const id in players) {
-		if (!backendPlayers[id]) {
-			delete players[id]
+	for (const id in frontEndPlayers) {
+		if (!backEndPlayers[id]) {
+			delete frontEndPlayers[id]
 		}
 	}
-
-	console.log(players)
 })
 
 let animationId
@@ -42,8 +40,8 @@ function animate() {
 	ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
 	ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-	for (const id in players) {
-		const player = players[id]
+	for (const id in frontEndPlayers) {
+		const player = frontEndPlayers[id]
 		player.draw()
 	}
 
